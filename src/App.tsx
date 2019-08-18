c6037651b07895adcb0b2a7d55bf4374b40d144d
Matthis Perrin <matthis.perrin@gmail.com>
2019-08-18 17:22:48 +0200

    Cleanup some files + Add prettier and tslint config

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
 
-const App: React.FC = () => {
+export const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppWrapper>
+      Edit <code>src/app.tsx</code> and save to reload.
+    </AppWrapper>
   );
-}
+};
 
-export default App;
+const AppWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  background-color: #282c34;
+  height: 100vh;
+  font-size: 20px;
+  color: white;
+`;
