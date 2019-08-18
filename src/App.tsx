bf7362b7bb0a6465e28dbf101e0846faa4921139
Matthis Perrin <matthis.perrin@gmail.com>
2019-08-19 00:19:15 +0200

    Update example with ParamWizz

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,18 @@ import React from 'react';
 import styled from 'styled-components';
 
 import {CSSReset} from './lib/core/css_reset';
-import {Wizz} from './lib/core/wizz';
+import {ParamWizz, Wizz} from './lib/core/wizz';
 
 const WithTodoList = Wizz<string[]>();
+
+interface Product {
+  id: string;
+  name: string;
+}
+const WithProduct = ParamWizz(
+  (products: Product[] | undefined, props: {productId: string}) =>
+    products && products.filter(p => p.id === props.productId)[0]
+);
 
 function addRandomTodo(): void {
   let currentTodoList = WithTodoList.getState();
@@ -24,23 +33,37 @@ export const App: React.FC = () => {
   return (
     <React.Fragment>
       <CSSReset />
-      <WithTodoList loader={<span>Loading...</span>}>
-        {todoList => (
-          <AppWrapper>
-            <TodoListTitle>TODO list</TodoListTitle>
-            {todoList.map((todo, index) => (
-              <div key={index}>{todo}</div>
-            ))}
-            <RandomTodoButton onClick={addRandomTodo}>Add random</RandomTodoButton>
-          </AppWrapper>
-        )}
-      </WithTodoList>
+      <AppWrapper>
+        <WithTodoList loader={<span>Loading...</span>}>
+          {todoList => (
+            <React.Fragment>
+              <TodoListTitle>TODO list</TodoListTitle>
+              {todoList.map((todo, index) => (
+                <div key={index}>{todo}</div>
+              ))}
+              <RandomTodoButton onClick={addRandomTodo}>Add random</RandomTodoButton>
+            </React.Fragment>
+          )}
+        </WithTodoList>
+        <WithProduct productId="0003" loader={<span>Loading product 0003...</span>}>
+          {product => <span>{`Product ${product.id} is ${product.name}`}</span>}
+        </WithProduct>
+      </AppWrapper>
     </React.Fragment>
   );
 };
 
-const FAKE_LOAD_TIME = 2000;
-setTimeout(() => WithTodoList.setState([]), FAKE_LOAD_TIME);
+const FAKE_LOAD_TIME = 1000;
+setTimeout(() => {
+  WithTodoList.setState(['Loaded todo 1', 'Loaded todo 2']);
+}, FAKE_LOAD_TIME);
+setTimeout(() => {
+  WithProduct.setState([
+    {id: '0001', name: 'P1'},
+    {id: '0002', name: 'P2'},
+    {id: '0003', name: 'P3'},
+  ]);
+}, 2 * FAKE_LOAD_TIME);
 
 const AppWrapper = styled.div`
   display: flex;
